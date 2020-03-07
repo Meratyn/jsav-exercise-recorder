de9d2b1a3177a742712c3cddb12abd4fb9fdab2a
giacomo <mariang1@aalto.fi>
2020-03-07 15:05:02 +0200

    minor stuff

--- exerciseRecorder.js
+++ exerciseRecorder.js
@@ -43,9 +43,9 @@ function passEvent(eventData) {
       anim_func.handleStateChange(exercise, eventData);
       break;
     case 'jsav-exercise-grade-button':
-      anim_func.handleGradeButtonClick(eventData);
       break;
     case 'jsav-exercise-grade':
+      anim_func.handleGradeButtonClick(eventData);
       def_func.setFinalGrade(eventData) && services.sendSubmission(submission.state());
       break;
     case 'jsav-recorded':
