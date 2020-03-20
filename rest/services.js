835efda68cb012568099126906eacc275a6b2715
Mariani Giacomo <mariang1@t31300-lr138.org.aalto.fi>
2020-03-20 15:53:38 +0200

    added post to window.parent

--- rest/services.js
+++ rest/services.js
@@ -3,17 +3,16 @@ const axios = require('axios');
 const serverAddress = 'http://localhost:3000';
 
 async function sendSubmission(data, url) {
-  if(url){
+  if(url === 'window'){
+    window.parent.postMessage(data, "*");
+  }
+  else {
     try {
       console.warn(`Sending submission data to server`)
       const response = await axios.post(`${serverAddress}/submissions`, data);
     } catch (err) {
       console.warn(`Failed sending submission to url ${url}: ${err}`)
     }
-  }
-  else {
-    console.warn(`No submission url provided, saving data in window global object`)
-    window.submission = data;
   }
 }
 
