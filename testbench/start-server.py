c2472750c61ef0af2b43311f138db48310336909
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2022-08-26 10:52:35 +0300

    start-server.py: parameter for HTTP port
    Added a command-line option to specify the HTTP port for the web server of
    the test bench. This is useful if one wants to run both A+ LMS at port 8000
    and this test bench at another port.

--- testbench/start-server.py
+++ testbench/start-server.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import http.server
 import socketserver
+import sys
 
 PORT = 8000
+# One can specify the HTTP server port as a command-line parameter, e.g.
+# ./start-server.py 8001
+if len(sys.argv) == 2:
+    PORT = int(sys.argv[1])
+
 
 Handler = http.server.SimpleHTTPRequestHandler
 
