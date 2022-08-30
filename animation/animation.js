3d64530d257fc952d5db6b66561106d9311c8d2f
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2022-08-30 15:58:03 +0300

    getClickedObject(): more comments
    animation/animation.js: function getClickedObject(): added more explanatory
    comments and issue #88.

--- animation/animation.js
+++ animation/animation.js
@@ -57,16 +57,16 @@ function submissionDataStructures() {
 }
 
 /**
- * Compare a singular edge object against an edge list to see if the 
+ * Compare a singular edge object against an edge list to see if the
  * style has been changed or not. Finds the matching edge in the last
- * known state and compares the style against that. 
+ * known state and compares the style against that.
  * @param edge object to see if it has been changed
  * @param lastState array of edge objects of the last known state
  * @returns whether the edge has been changed or not.
  */
 function edgeChanged(edge, lastState) {
   for (var i = 0; i < lastState.length; i++) {
-    if (lastState[i].id === edge.id && 
+    if (lastState[i].id === edge.id &&
         lastState[i].style !== edge.style) {
           return true;
     }
@@ -76,46 +76,66 @@ function edgeChanged(edge, lastState) {
 
 /**
  * Grab the last known state and compare it against the current state to
- * attempt to figure out which edge has been clicked. 
+ * attempt to figure out which edge has been clicked.
  * TODO: Generalise to non-Dijkstra/non-edge-in-graph clicked version.
  * @param dsState data structure state object
- * @returns the clicked egde id, or undefined if no edge has been changed. 
- * Undefined will throw a JAAL1.1 validation error
+ * @returns the clicked egde id, or undefined if no edge has been changed.
  */
 function getClickedObject(dsState) {
-  /**
-   * Check if this step is the first one in the array. If so, use the 
-   * initial state to check against. If not, use the last recorded state.
-   */
-  const lastState = (submission.state().animation.length === 0) 
-                    ? submission.state().initialState.dataStructures[0]
-                    : state;
-  // Update state to the current state
-  state = dsState[0];
-  const lastEdgeList = lastState.edge;
-  const currentEdges = state.edge;
+  // Check if this step is the first one in the array. If so, use the
+  // initial state to check against. If not, use the last recorded state.
+  // Assume that:
+  // - dataStructures[0] is a JSAV graph
+  // - dataStructures[0] is the object that has been clicked
+  const dsIndex = 0;
 
-  for (var i = 0; i < currentEdges.length; i++) {
-    if (edgeChanged(currentEdges[i], lastEdgeList)) {
-      return currentEdges[i].id;
+  // Last state of the data structure
+  let lastState = undefined;
+  if (submission.state().animation.length === 0) {
+    lastState = submission.state().initialState.dataStructures[dsIndex];
+  }
+  else {
+    lastState = state;
+  }
+
+  // Current state of the data structure
+  state = dsState[dsIndex];
+  if (lastState.dsClass === "graph") {
+    const lastEdgeList = lastState.edge;
+    const currentEdges = state.edge;
+    for (var i = 0; i < currentEdges.length; i++) {
+      console.log(lastEdgeList[i].id + " " + lastEdgeList[i].style + " " +
+                  currentEdges[i].id + " " + currentEdges[i].style + " ");
+      if (edgeChanged(currentEdges[i], lastEdgeList)) {
+        return currentEdges[i].id;
+      }
     }
   }
-  
+  else {
+    throw new Error("Implementation missing for comparison of " +
+      "JAAL data structure class:" + lastState.dsClass);
+  }
+
+  // Fallback to generate a JAAL validation error
   return undefined;
 }
 
 /**
- * Record the current step as valid JAAL 1.1 and add this to the submission. 
+ * Record the current step as JAAL and add this to the submission.
  * @param eventData as passed by JSAV.
- * @param dataStructuresState the state of the current dataStructure. 
+ * @param dataStructuresState the state of the current dataStructure.
  * @param svgImage generated svg image of the current state.
  */
 function addStepToSubmission(eventData, dataStructuresState, svgImage) {
+  // Map JSAV event to JAAL event type:
+  // "jsav-exercise-undo" -> "undo"
+  // everything else      -> "click"
   const type = eventData.type === 'jsav-exercise-undo' ? 'undo' : 'click';
+
   const animation = submission.state().animation;
-  const currentStep = eventData.currentStep || 
+  const currentStep = eventData.currentStep ||
                       animation[animation.length - 1].currentStep +1;
-  
+
   const startTime = submission.state().metadata.recordingStarted;
   const tstamp = eventData.tstamp || new Date();
 
