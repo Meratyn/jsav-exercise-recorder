ad5cfe017122da94127022154b484b59b65c1a69
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2022-08-30 13:29:41 +0300

    JAAL as git submodule
    JAAL JSON Schema specification was added as a git submodule. This allows
    to bind a particular version of the JAAL schema to the JSAV Exercise Recorder
    version by git commit accuracy.

--- validation/update-schemas.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# A quick and dirty script to update the schema files. 
-# It assumes that JAAL is in the same folder as jsav-exercise-recorder
-rm -r ./schemas/*
-cp ../../JAAL/spec/schemas/* schemas/
